8bdf9da45b0b4bb692bde00d863419357a710b8a
Your Name <you@example.com>
2022-07-23 21:27:52 -0600

    stuck unsure of what to do

---
+++ js/app.js
@@ -0,0 +1,67 @@
+
+
+
+let selection_one = 
+    {
+        title: `Show One`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+
+    let selection_two =
+    {
+        title: `Show two`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+
+    let selection_three =
+    {
+        title: `Show three`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+    let selection_four =
+    {
+        title: `Show four`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+
+    let selection_five =
+    {
+        title: `Show five`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+
+    let selection_six =
+    {
+        title: `Show six`,
+        price: 5,
+        image_url: `/shawshank.jpg`,
+        description: `show or movie for rent`
+    };
+
+function buttonOne(details) {   
+    let selection_one_json = JSON.stringify(selection_one);
+    Cookies.set(`movie_one`, selection_one_json);
+   
+for (i = 0; i = selection_one; i++) {
+    let varible =  [];
+   variable.push(selection_one_json);
+}
+}
+
+let button_one = document.getElementById(`button_one`)
+button_one.addEventListener(`click`, buttonOne)
