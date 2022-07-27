c0ee159fa697a6a685e916e691c5dbb4d41588f4
Your Name <you@example.com>
2022-07-26 23:12:08 -0600

    added unique selection of movies

--- js/app.js
+++ js/app.js
@@ -1,77 +1,86 @@
 
 
 
-let selection_one =
+let selection_one =                     // created variable that is an object that contains key values below for the six selections
 {
     title: `Shawshank Redemption`,
     price: 5,
     image_url: `/shawshank.jpg`,
     description: `Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.
-                Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit`
+ Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of 
+ a crime he did not commit`
 };
 
 
-let selection_two =
+let selection_two =                     
 {
-    title: `Show two`,
+    title: `The Dark Knight`,
     price: 5,
-    image_url: `/shawshank.jpg`,
-    description: `show or movie for rent`
+    image_url: `/darkknight.jpg`,
+    description: `When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one
+    of the greatest psychological and physical tests of his ability to fight injustice`
 };
 
 
-let selection_three =
+let selection_three =                   
 {
-    title: `Show three`,
+    title: `The godfather`,
     price: 5,
-    image_url: `/shawshank.jpg`,
-    description: `show or movie for rent`
+    image_url: `/godfather.jpg`,
+    description: `The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his
+    clandestine empire to his reluctant youngest son`
 };
 
-let selection_four =
+let selection_four =                    
 {
-    title: `Show four`,
+    title: `The Green mile`,
     price: 5,
-    image_url: `/shawshank.jpg`,
-    description: `show or movie for rent`
+    image_url: `/green mile.jpg`,
+    description: `The lives of guards on Death Row are affected by one of their charges: a black man accused of child
+    murder and rape, yet who has a mysterious gift.`
 };
 
 
-let selection_five =
+let selection_five =                    
 {
-    title: `Show five`,
+    title: `Inception`,
     price: 5,
-    image_url: `/shawshank.jpg`,
-    description: `show or movie for rent`
+    image_url: `/inception.jpg`,
+    description: `>A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse
+    task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his
+    team to disaster`
 };
 
 
-let selection_six =
+let selection_six =                    
 {
-    title: `Show six`,
+    title: `The Matrix`,
     price: 5,
-    image_url: `/shawshank.jpg`,
-    description: `show or movie for rent`
+    image_url: `/matrix.jpg`,
+    description: `When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking
+    truth--the life he knows is the elaborate deception of an evil cyber-intelligence.`
 };
 
 
 
-let movie_array = [];
-let selections = Cookies.get(`selections`)
-if (selections !== undefined) {
-    movie_array = JSON.parse(selections);
+
+
+let movie_array = [];                                           // made variable movie_array which is a empty array
+let selections = Cookies.get(`selections`)                      // gets the selections cookie and stores it as the variable selections
+if (selections !== undefined) {                                 // if selections is defined (cookie exists) 
+    movie_array = JSON.parse(selections);                       // the movie_array (array) is = to the selections cookie
 }
 
-function buttonOne(details) {
+function buttonOne(details) {                                   // all the functions below act the same way just w/ diff variables
 
-    movie_array.push(selection_one)
+    movie_array.push(selection_one)                             // pushes the variable selection_one (obj) into the array w/ the variable movie_array
 
-    let selection_one_json = JSON.stringify(movie_array);
-    Cookies.set(`selections`, selection_one_json);
+    let selection_one_json = JSON.stringify(movie_array);       // puts the movie_array into a string and stores it as the variable selection_one_json
+    Cookies.set(`selections`, selection_one_json);              // sets the cookies name to selections and value to selection_one_json (string)
 }
 
-let button_one = document.getElementById(`button_one`)
-button_one.addEventListener(`click`, buttonOne)
+let button_one = document.getElementById(`button_one`)          // gets the id button_one and stores it as the variable button_one
+button_one.addEventListener(`click`, buttonOne)                 // when button_one is clicked it will call the function buttonOne
 
 
 
--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,15 @@
-let movie_selection = Cookies.get(`selections`)
-let movie_array = JSON.parse(movie_selection);
+let movie_selection = Cookies.get(`selections`)         // gets the selections cookie and stores it as the variable movie_selection
+let movie_array = JSON.parse(movie_selection);          // the movie_array (array) is = movie_selection (turns string into array)
 
-for(i = 0; i < movie_array.length; i++) {
+for(let i = 0; i < movie_array.length; i++) {           // loops through the movie_array if the counter is less, adds +1 after,
 
-document.body.insertAdjacentHTML (`beforeend`,
+document.body.insertAdjacentHTML (`beforeend`,          // inject section tag into page that will display the movie_array title, image and desc depending on what the counter is
 
 `<section ">
 <h1>${movie_array[i][`title`]}</h1>
 <img src="${movie_array[i][`image_url`]}" >
 <p>${movie_array[i][`description`]}</p>
-
+<h4>${movie_array[i][`price`]}</h4>
 
 </section>`);
 }
